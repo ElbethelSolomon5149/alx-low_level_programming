10e3775411032c8fe9065fba1afb92c5bdc7a496
Elbethel Solomon <solomonelbethel.com>
2023-03-19 13:35:39 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-
+#include <studio.h>
+/**
+ * main - Entry point
+ * description - the entry point to the program
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n ");
+	return (0);
+}
