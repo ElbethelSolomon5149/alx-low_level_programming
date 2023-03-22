d6daef3f3c8c75a44c8c21b11fbc472fdeb6851d
Elbethel Solomon <solomonelbethel.com>
2023-03-21 19:28:43 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - function to print lower case
+ * Description: parameter to be printed
+ * Return: 1 if it is a lower case and 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
