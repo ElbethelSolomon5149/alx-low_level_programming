9070703a5ac9e777b6c66c77a270386d3ce7a55a
Elbethel Solomon <solomonelbethel.com>
2023-03-23 19:36:47 -0700

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - cases an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
