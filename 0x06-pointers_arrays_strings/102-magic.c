024787dec95d0e851557cd5bd739afeed884e2d7
Elbethel Solomon <solomonelbethel.com>
2023-03-30 20:16:59 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints 98 followed by new line
+ * Return: always 0
+ * description: printing 98
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
