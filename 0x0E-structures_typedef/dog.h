f0fa1b3fcc46ed6de8a113c7dfaa5b12f79a7225
Elbethel Solomon <solomonelbethel.com>
2023-04-18 19:25:36 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - strucy for dog
+ * Description: struct for dogs
+ * @name: for name
+ * @age: for age
+ * @owner:for char owner
+ * Return: struct
+ */
 struct dog
 {
 	char *name;
