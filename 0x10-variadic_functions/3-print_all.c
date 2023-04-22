5d2df7e46211168555d83bf5338ffe1d816e064d
Elbethel Solomon <solomonelbethel.com>
2023-04-21 18:54:06 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,5 @@
 #include "variadic_functions.h"
-#include <strdarg.h>
+#include <stdarg.h>
 #include <stdio.h>
 /**
  * print_all - printr anything
@@ -23,6 +23,7 @@ void print_all(const char *const format, ...)
 					break;
 				case'i':
 					printf("%s%d", sep, va_arg(list, int));
+					break;
 				case'f':
 					printf("%s%f", sep, va_arg(list, double));
 					break;
@@ -37,7 +38,7 @@ void print_all(const char *const format, ...)
 					continue;
 			}
 			sep = ",";
-			i++
+			i++;
 		}
 	}
 	printf("\n");
